f59efb5f3abd35adbe93ea23e1aade02dc2ce86b
davidburnazyan <david.burnazyan.96@gmail.com>
2020-11-16 22:56:33 +0400

    change mongoose

--- app.js
+++ app.js
@@ -18,10 +18,7 @@ const productRoutes = require('./api/routes/product');
 mongoose.connect(
     `mongodb+srv://David:${process.env.MONGO_ATLAS_PW}@graphql.p4ybv.mongodb.net/GraphQLDB?retryWrites=true&w=majority`,
     {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
+        useNewUrlParser : true
     }
 )
 
